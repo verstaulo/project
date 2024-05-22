6653a50e6a7e369f78ccb0c2366b1f81aa6a9f29
Myltik <verstaulo@gamil.com>
2024-05-22 18:40:27 +0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/project/',
+  base: '/project/', // при деплое на кастомный домен установить путь base:'./'
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
